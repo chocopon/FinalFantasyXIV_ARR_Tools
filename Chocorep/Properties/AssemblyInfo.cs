f1480aa3e9d522af6118db70e5e4cb8c94aa0128
chocopon <azuma52s@gmail.com>
2013-11-22 09:28:53 +0900

    不足分ついか。ソリューション名変更

---
+++ Chocorep/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("Chocorep(FF14 A Realm Reborn)")]
+[assembly: AssemblyDescription("FINAL FANTASY XIV - A Realm Reborn 用 Repソフト")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Chiro&Choco")]
+[assembly: AssemblyProduct("Chocorep for FF14 (A Realm Reborn)")]
+[assembly: AssemblyCopyright("Copyright © Chiro&&Choco 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// 次の GUID は、このプロジェクトが COM に公開される場合の、typelib の ID です
+[assembly: Guid("4c463d49-ec9b-4ec6-983b-99b9c62877b5")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1.1.3")]
+[assembly: AssemblyFileVersion("2.1.1.3")]
