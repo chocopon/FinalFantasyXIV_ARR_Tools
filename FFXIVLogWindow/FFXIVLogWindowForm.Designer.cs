39559b64643ad3f669d5d1b4f9d80570a6b3e19f
chocopon <azuma52s@gmail.com>
2013-11-27 09:45:46 +0900

    FFXIVLogWindow追加
    ゲーム内のログを別窓で表示するツール

---
+++ FFXIVLogWindow/FFXIVLogWindowForm.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace FFXIVLogWindow
+{
+    partial class FFXIVLogWindowForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EmoteBox = new System.Windows.Forms.CheckBox();
+            this.PartyBox = new System.Windows.Forms.CheckBox();
+            this.YellBox = new System.Windows.Forms.CheckBox();
+            this.ShoutBox = new System.Windows.Forms.CheckBox();
+            this.SayBox = new System.Windows.Forms.CheckBox();
+            this.LS8Box = new System.Windows.Forms.CheckBox();
+            this.LS7Box = new System.Windows.Forms.CheckBox();
+            this.LS6Box = new System.Windows.Forms.CheckBox();
+            this.LS5Box = new System.Windows.Forms.CheckBox();
+            this.LS4Box = new System.Windows.Forms.CheckBox();
+            this.LS3Box = new System.Windows.Forms.CheckBox();
+            this.LS2Box = new System.Windows.Forms.CheckBox();
+            this.LS1Box = new System.Windows.Forms.CheckBox();
+            this.FCBox = new System.Windows.Forms.CheckBox();
+            this.LogBodyBox = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.保存されたログを開くFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.データDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fFXIVで保存されたログからインポートIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.実行中のゲームからインポートLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenLogFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.EmoteBox);
+            this.panel1.Controls.Add(this.PartyBox);
+            this.panel1.Controls.Add(this.YellBox);
+            this.panel1.Controls.Add(this.ShoutBox);
+            this.panel1.Controls.Add(this.SayBox);
+            this.panel1.Controls.Add(this.LS8Box);
+            this.panel1.Controls.Add(this.LS7Box);
+            this.panel1.Controls.Add(this.LS6Box);
+            this.panel1.Controls.Add(this.LS5Box);
+            this.panel1.Controls.Add(this.LS4Box);
+            this.panel1.Controls.Add(this.LS3Box);
+            this.panel1.Controls.Add(this.LS2Box);
+            this.panel1.Controls.Add(this.LS1Box);
+            this.panel1.Controls.Add(this.FCBox);
+            this.panel1.Location = new System.Drawing.Point(12, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(507, 51);
+            this.panel1.TabIndex = 0;
+            // 
+            // EmoteBox
+            // 
+            this.EmoteBox.AutoSize = true;
+            this.EmoteBox.Location = new System.Drawing.Point(348, 7);
+            this.EmoteBox.Name = "EmoteBox";
+            this.EmoteBox.Size = new System.Drawing.Size(56, 16);
+            this.EmoteBox.TabIndex = 13;
+            this.EmoteBox.Text = "Emote";
+            this.EmoteBox.UseVisualStyleBackColor = true;
+            // 
+            // PartyBox
+            // 
+            this.PartyBox.AutoSize = true;
+            this.PartyBox.Location = new System.Drawing.Point(201, 7);
+            this.PartyBox.Name = "PartyBox";
+            this.PartyBox.Size = new System.Drawing.Size(51, 16);
+            this.PartyBox.TabIndex = 12;
+            this.PartyBox.Text = "Party";
+            this.PartyBox.UseVisualStyleBackColor = true;
+            // 
+            // YellBox
+            // 
+            this.YellBox.AutoSize = true;
+            this.YellBox.Location = new System.Drawing.Point(299, 7);
+            this.YellBox.Name = "YellBox";
+            this.YellBox.Size = new System.Drawing.Size(43, 16);
+            this.YellBox.TabIndex = 11;
+            this.YellBox.Text = "Yell";
+            this.YellBox.UseVisualStyleBackColor = true;
+            // 
+            // ShoutBox
+            // 
+            this.ShoutBox.AutoSize = true;
+            this.ShoutBox.Location = new System.Drawing.Point(103, 7);
+            this.ShoutBox.Name = "ShoutBox";
+            this.ShoutBox.Size = new System.Drawing.Size(53, 16);
+            this.ShoutBox.TabIndex = 10;
+            this.ShoutBox.Text = "Shout";
+            this.ShoutBox.UseVisualStyleBackColor = true;
+            // 
+            // SayBox
+            // 
+            this.SayBox.AutoSize = true;
+            this.SayBox.Location = new System.Drawing.Point(54, 7);
+            this.SayBox.Name = "SayBox";
+            this.SayBox.Size = new System.Drawing.Size(43, 16);
+            this.SayBox.TabIndex = 9;
+            this.SayBox.Text = "Say";
+            this.SayBox.UseVisualStyleBackColor = true;
+            // 
+            // LS8Box
+            // 
+            this.LS8Box.AutoSize = true;
+            this.LS8Box.Location = new System.Drawing.Point(348, 29);
+            this.LS8Box.Name = "LS8Box";
+            this.LS8Box.Size = new System.Drawing.Size(43, 16);
+            this.LS8Box.TabIndex = 8;
+            this.LS8Box.Text = "LS8";
+            this.LS8Box.UseVisualStyleBackColor = true;
+            // 
+            // LS7Box
+            // 
+            this.LS7Box.AutoSize = true;
+            this.LS7Box.Location = new System.Drawing.Point(299, 29);
+            this.LS7Box.Name = "LS7Box";
+            this.LS7Box.Size = new System.Drawing.Size(43, 16);
+            this.LS7Box.TabIndex = 7;
+            this.LS7Box.Text = "LS7";
+            this.LS7Box.UseVisualStyleBackColor = true;
+            // 
+            // LS6Box
+            // 
+            this.LS6Box.AutoSize = true;
+            this.LS6Box.Location = new System.Drawing.Point(240, 29);
+            this.LS6Box.Name = "LS6Box";
+            this.LS6Box.Size = new System.Drawing.Size(43, 16);
+            this.LS6Box.TabIndex = 6;
+            this.LS6Box.Text = "LS6";
+            this.LS6Box.UseVisualStyleBackColor = true;
+            // 
+            // LS5Box
+            // 
+            this.LS5Box.AutoSize = true;
+            this.LS5Box.Location = new System.Drawing.Point(201, 29);
+            this.LS5Box.Name = "LS5Box";
+            this.LS5Box.Size = new System.Drawing.Size(43, 16);
+            this.LS5Box.TabIndex = 5;
+            this.LS5Box.Text = "LS5";
+            this.LS5Box.UseVisualStyleBackColor = true;
+            // 
+            // LS4Box
+            // 
+            this.LS4Box.AutoSize = true;
+            this.LS4Box.Location = new System.Drawing.Point(152, 29);
+            this.LS4Box.Name = "LS4Box";
+            this.LS4Box.Size = new System.Drawing.Size(43, 16);
+            this.LS4Box.TabIndex = 4;
+            this.LS4Box.Text = "LS4";
+            this.LS4Box.UseVisualStyleBackColor = true;
+            // 
+            // LS3Box
+            // 
+            this.LS3Box.AutoSize = true;
+            this.LS3Box.Location = new System.Drawing.Point(103, 29);
+            this.LS3Box.Name = "LS3Box";
+            this.LS3Box.Size = new System.Drawing.Size(43, 16);
+            this.LS3Box.TabIndex = 3;
+            this.LS3Box.Text = "LS3";
+            this.LS3Box.UseVisualStyleBackColor = true;
+            // 
+            // LS2Box
+            // 
+            this.LS2Box.AutoSize = true;
+            this.LS2Box.Location = new System.Drawing.Point(54, 29);
+            this.LS2Box.Name = "LS2Box";
+            this.LS2Box.Size = new System.Drawing.Size(43, 16);
+            this.LS2Box.TabIndex = 2;
+            this.LS2Box.Text = "LS2";
+            this.LS2Box.UseVisualStyleBackColor = true;
+            // 
+            // LS1Box
+            // 
+            this.LS1Box.AutoSize = true;
+            this.LS1Box.Location = new System.Drawing.Point(5, 29);
+            this.LS1Box.Name = "LS1Box";
+            this.LS1Box.Size = new System.Drawing.Size(43, 16);
+            this.LS1Box.TabIndex = 1;
+            this.LS1Box.Text = "LS1";
+            this.LS1Box.UseVisualStyleBackColor = true;
+            // 
+            // FCBox
+            // 
+            this.FCBox.AutoSize = true;
+            this.FCBox.Location = new System.Drawing.Point(5, 7);
+            this.FCBox.Name = "FCBox";
+            this.FCBox.Size = new System.Drawing.Size(39, 16);
+            this.FCBox.TabIndex = 0;
+            this.FCBox.Text = "FC";
+            this.FCBox.UseVisualStyleBackColor = true;
+            // 
+            // LogBodyBox
+            // 
+            this.LogBodyBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LogBodyBox.BackColor = System.Drawing.Color.Black;
+            this.LogBodyBox.Location = new System.Drawing.Point(12, 86);
+            this.LogBodyBox.Name = "LogBodyBox";
+            this.LogBodyBox.ReadOnly = true;
+            this.LogBodyBox.Size = new System.Drawing.Size(507, 503);
+            this.LogBodyBox.TabIndex = 1;
+            this.LogBodyBox.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 607);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(528, 23);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(134, 18);
+            this.toolStripStatusLabel.Text = "toolStripStatusLabel1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem,
+            this.データDToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(528, 26);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.保存されたログを開くFToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(85, 22);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 保存されたログを開くFToolStripMenuItem
+            // 
+            this.保存されたログを開くFToolStripMenuItem.Name = "保存されたログを開くFToolStripMenuItem";
+            this.保存されたログを開くFToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.保存されたログを開くFToolStripMenuItem.Text = "ログを開く(&O)";
+            // 
+            // データDToolStripMenuItem
+            // 
+            this.データDToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fFXIVで保存されたログからインポートIToolStripMenuItem,
+            this.実行中のゲームからインポートLToolStripMenuItem});
+            this.データDToolStripMenuItem.Name = "データDToolStripMenuItem";
+            this.データDToolStripMenuItem.Size = new System.Drawing.Size(75, 22);
+            this.データDToolStripMenuItem.Text = "データ(&D)";
+            // 
+            // fFXIVで保存されたログからインポートIToolStripMenuItem
+            // 
+            this.fFXIVで保存されたログからインポートIToolStripMenuItem.Name = "fFXIVで保存されたログからインポートIToolStripMenuItem";
+            this.fFXIVで保存されたログからインポートIToolStripMenuItem.Size = new System.Drawing.Size(306, 22);
+            this.fFXIVで保存されたログからインポートIToolStripMenuItem.Text = "FFXIVで保存されたログからインポート(&I)";
+            this.fFXIVで保存されたログからインポートIToolStripMenuItem.Click += new System.EventHandler(this.fFXIVで保存されたログからインポートIToolStripMenuItem_Click);
+            // 
+            // 実行中のゲームからインポートLToolStripMenuItem
+            // 
+            this.実行中のゲームからインポートLToolStripMenuItem.Name = "実行中のゲームからインポートLToolStripMenuItem";
+            this.実行中のゲームからインポートLToolStripMenuItem.Size = new System.Drawing.Size(306, 22);
+            this.実行中のゲームからインポートLToolStripMenuItem.Text = "実行中のゲームからインポート(&L)";
+            // 
+            // FFXIVLogWindowForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 630);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.LogBodyBox);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FFXIVLogWindowForm";
+            this.Text = "FFXIVLogWindow";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox EmoteBox;
+        private System.Windows.Forms.CheckBox PartyBox;
+        private System.Windows.Forms.CheckBox YellBox;
+        private System.Windows.Forms.CheckBox ShoutBox;
+        private System.Windows.Forms.CheckBox SayBox;
+        private System.Windows.Forms.CheckBox LS8Box;
+        private System.Windows.Forms.CheckBox LS7Box;
+        private System.Windows.Forms.CheckBox LS6Box;
+        private System.Windows.Forms.CheckBox LS5Box;
+        private System.Windows.Forms.CheckBox LS4Box;
+        private System.Windows.Forms.CheckBox LS3Box;
+        private System.Windows.Forms.CheckBox LS2Box;
+        private System.Windows.Forms.CheckBox LS1Box;
+        private System.Windows.Forms.CheckBox FCBox;
+        private System.Windows.Forms.RichTextBox LogBodyBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 保存されたログを開くFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem データDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fFXIVで保存されたログからインポートIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 実行中のゲームからインポートLToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog OpenLogFolderBrowserDialog;
+    }
+}
+
---
+++ FFXIVLogWindow/FFXIVLogWindowForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using LogManipulate;
+
+namespace FFXIVLogWindow
+{
+    public partial class FFXIVLogWindowForm : Form
+    {
+        public FFXIVLogWindowForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fFXIVで保存されたログからインポートIToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenLogFolderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                FFXIVLogFileReader filereader = new FFXIVLogFileReader(OpenLogFolderBrowserDialog.SelectedPath);
+                int count = 0;
+                foreach (FFXIVLog log in filereader.GetLogs())
+                {
+                    LogBodyBox.SelectionColor = GetColorFromLog(log);
+                    Font baseFont = LogBodyBox.SelectionFont;
+                    Font fnt = new Font(baseFont.FontFamily,
+                        baseFont.Size,
+                        baseFont.Style | FontStyle.Bold);
+
+                    //Fontを変更する
+                    LogBodyBox.SelectionFont = fnt;
+                    //文字列を挿入する
+                    LogBodyBox.SelectedText = String.Format("[{0}]{1}:{2}\r\n", log.TimeStampServerTime.ToString("MM/dd HH:mm:ss"), log.LogTypeHexString, log.LogBodyReplaceTabCode);
+
+                    baseFont.Dispose();
+                    fnt.Dispose();
+
+                    count++;
+                    if (count > 1000)
+                        break;
+                }
+            }
+        }
+
+        private Color GetColorFromLog(FFXIVLog log)
+        {
+            if (log.LogTypeHexString == "0018")//fc
+            {
+                return Color.LightGreen;
+            }
+            else if (log.LogTypeHexString =="000B")//shout
+            {
+                return Color.DarkOrange;
+            }
+            else if (log.LogTypeHexString == "000E")//Party
+            {
+                return Color.LightSeaGreen;
+            }
+            else if (log.LogTypeHexString == "0010" || //LS
+                log.LogTypeHexString == "0011" ||
+                log.LogTypeHexString == "0012" ||
+                log.LogTypeHexString == "0013" ||
+                log.LogTypeHexString == "0014" ||
+                log.LogTypeHexString == "0015" ||
+                log.LogTypeHexString == "0016" ||
+                log.LogTypeHexString == "0017")
+            {
+                return Color.Green;
+            }
+            return Color.WhiteSmoke;
+
+        }
+    }
+}
